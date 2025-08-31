2e6e47ccbbf915fd21484708e3f5f02861acce40
Avantika Shekhawat <avantika.shekhawat20@gmail.com>
2025-08-31 22:24:19 +0530

    payment processing

--- server.js
+++ server.js
@@ -33,13 +33,16 @@ app.use((req, res, next) => {
     "https://dlcproperties.in",
     "http://localhost:5173"
   ];
+
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
+
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
+
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
